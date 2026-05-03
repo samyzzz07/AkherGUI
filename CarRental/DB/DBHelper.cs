75364eeb9ec3542e351f0097ac54a25ef1c2a360
samyzzz07 <mohamed.ahmed.samy2007@gmail.com>
2026-05-04 01:58:57 +0300

    akher error

--- CarRental/DB/DBHelper.cs
+++ CarRental/DB/DBHelper.cs
@@ -4,5 +4,5 @@ namespace CarRental.DB;
 
 public static class DBHelper
 {
-    public static string ConnStr = "Data Source=.;Initial Catalog=DriveBase;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+    public static string ConnStr = ConfigurationManager.ConnectionStrings["CarRentalDB"].ConnectionString;
 }
